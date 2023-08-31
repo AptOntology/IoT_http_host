a0c587d30899fe54c519ad36ff3959160408ff93
AptOntology <31258046+AptOntology@users.noreply.github.com>
2023-08-31 00:05:59 -0500

    Create data.php

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php
+
+$chipId = $_GET["chipid"];
+$theCmd = $_GET["cmd"];
+$theResult = $_GET["data"];
+
+$servername = "localhost";
+$username = "writeruser";
+$password = "Password1";
+$dbname = "databaseName";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT into tech_galaxy_net.output(id,command,result) values ('" . substr($chipId,0,100) . "','" . subStr($theCmd,0,100) . "','" . subStr($theResult,0,100) . "')";
+
+//echo $sql;
+$result = $conn->query($sql);
+echo $result
+$conn->close();
+
+?>
