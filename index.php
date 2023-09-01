9f84acbf051db49b554014a2517b75315f292ff5
AptOntology <31258046+AptOntology@users.noreply.github.com>
2023-08-31 20:45:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+$servername = "localhost";
+$username = "readeruser";
+$password = "Password1";
+$dbname = "databaseName";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from sensors order by id";
+
+$result = $conn->query($sql);
+
+echo "<h1>Command</h1>";
+if ($result->num_rows > 0) {
+   while($row = $result->fetch_assoc()) {
+   echo "<p>";
+   echo $row["id"];
+   echo " | ";
+   echo $row["command"];
+   echo "</p>";
+  }
+}
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from output ORDER BY datetime desc, id desc LIMIT 50";
+
+$result = $conn->query($sql);
+
+echo "<h1>Result</h1>";
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+   echo "<p>";
+   echo $row["datetime"];
+   echo " | ";
+   echo $row["id"];
+   echo " | ";
+   echo $row["command"];
+   echo "</p>";
+  }
+}
+
+?>
